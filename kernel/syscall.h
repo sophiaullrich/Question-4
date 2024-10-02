0348338d1623f202940a42fd5954c06a0676eeaa
sophiaullrich <105826768+sophiaullrich@users.noreply.github.com>
2024-10-02 17:19:35 +1300

    Add files via upload

---
+++ kernel/syscall.h
@@ -0,0 +1,60 @@
+#ifndef _SYSCALL_H_
+#define _SYSCALL_H_
+
+// Include system call numbers
+#define SYS_fork    1
+#define SYS_exit    2
+#define SYS_wait    3
+#define SYS_pipe    4
+#define SYS_read    5
+#define SYS_kill    6
+#define SYS_exec    7
+#define SYS_fstat   8
+#define SYS_chdir   9
+#define SYS_dup    10
+#define SYS_getpid 11
+#define SYS_sbrk   12
+#define SYS_sleep  13
+#define SYS_uptime 14
+#define SYS_open   15
+#define SYS_write  16
+#define SYS_mknod  17
+#define SYS_unlink 18
+#define SYS_link   19
+#define SYS_mkdir  20
+#define SYS_close  21
+#define SYS_ps     22
+#define SYS_pageAccess 23
+#define SYS_setnice 24
+
+#ifndef __ASSEMBLER__  // Only include the C declarations in C files
+
+// C declarations for system calls
+extern uint64 sys_fork(void);
+extern uint64 sys_exit(void);
+extern uint64 sys_wait(void);
+extern uint64 sys_pipe(void);
+extern uint64 sys_read(void);
+extern uint64 sys_kill(void);
+extern uint64 sys_exec(void);
+extern uint64 sys_fstat(void);
+extern uint64 sys_chdir(void);
+extern uint64 sys_dup(void);
+extern uint64 sys_getpid(void);
+extern uint64 sys_sbrk(void);
+extern uint64 sys_sleep(void);
+extern uint64 sys_uptime(void);
+extern uint64 sys_open(void);
+extern uint64 sys_write(void);
+extern uint64 sys_mknod(void);
+extern uint64 sys_unlink(void);
+extern uint64 sys_link(void);
+extern uint64 sys_mkdir(void);
+extern uint64 sys_close(void);
+extern uint64 sys_ps(void);
+extern uint64 sys_pageAccess(void);
+extern uint64 sys_setnice(void);
+
+#endif  // __ASSEMBLER__
+
+#endif  // _SYSCALL_H_
